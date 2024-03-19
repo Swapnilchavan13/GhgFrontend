a1770c3841c4ad8f3ed1159ded4d978928a9e3e9
swapnil chavan <chavanswapnil822@gmail.com>
2024-03-19 17:03:43 +0530

    updated code

--- ghgfr/src/Component/Myemission.jsx
+++ ghgfr/src/Component/Myemission.jsx
@@ -111,11 +111,11 @@ export const Myemission = () => {
           <tr>
             <td colSpan={scopes.length + 1}><strong>Total</strong></td>
             <td>
-            <strong>
-  {Object.values(aggregatedData).reduce((total, userData) => {
-    return total + Object.values(userData).reduce((acc, val) => acc + val, 0);
-  }, 0).toFixed(2)}
-</strong>
+              <strong>
+                {Object.values(aggregatedData).reduce((total, userData) => {
+                  return total + Object.values(userData).reduce((acc, val) => acc + val, 0);
+                }, 0).toFixed(2)}
+              </strong>
 
             </td>
           </tr>
@@ -159,19 +159,19 @@ export const Myemission = () => {
                 <td>{item.sku}</td>
                 <td>{item.unit}</td>
                 <td>
-                <a href={`http://62.72.59.146:8080/${item.emission}`} target="_blank" rel="noopener noreferrer">
-                  <img style={{ width: '80px' }} src={`http://62.72.59.146:8080/${item.emission}`} alt="Image" />
-                 </a>
+                  <a href={`http://62.72.59.146:8080/${item.emission}`} target="_blank" rel="noopener noreferrer">
+                    <img style={{ width: '80px' }} src={`http://62.72.59.146:8080/${item.emission}`} alt="Image" />
+                  </a>
                 </td>
                 <td>{item.date}</td>
                 <td>{item.date1}</td>
                 <td>
-  {typeof item.result === 'number'
-    ? parseFloat(item.result.toFixed(2))
-    : typeof item.result === 'string'
-    ? parseFloat(item.result.match(/-?\d+(?:\.\d{0,2})?/)[0]).toFixed(2)
-    : item.result}
-</td>
+                  {typeof item.result === 'number'
+                    ? parseFloat(item.result.toFixed(2))
+                    : typeof item.result === 'string'
+                      ? parseFloat(item.result.match(/-?\d+(?:\.\d{0,2})?/)[0]).toFixed(2)
+                      : item.result}
+                </td>
 
               </tr>
             ))}
