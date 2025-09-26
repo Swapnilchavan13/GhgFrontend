011249fcf626aa98bc3e0b11c9eef43b3d1cdcda
Swapnil Chavan <chavanswapnil822@gmail.com>
2025-09-26 19:01:49 +0530

    updated code

--- ghgfr/src/Pages/Footer.jsx
+++ ghgfr/src/Pages/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/Footer.css";
+import "../styles/footer.css";
 // import { FaPaperPlane, FaEnvelope } from "react-icons/fa";
 
 export const Footer = () => {
